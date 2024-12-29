eed926fb7caa60b0dd814529106197bf89c6db54
Nihar Hegde <niharhegde163@gmail.com>
2024-12-29 18:43:59 +0530

    adding assets and fonts and debugging the fonts

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,17 +3,27 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+    <View className="p-4">
+      {/* Test 1: Direct font-family */}
+      <Text className="my-2 text-3xl" style={{ fontFamily: "Rubik-Regular" }}>
+        Test 1: Direct Style
+      </Text>
+
+      {/* Test 2: NativeWind class */}
+      <Text className="my-2 text-3xl font-rubik">Test 2: NativeWind Class</Text>
+
+      <Text className="font-rubik text-3xl "> Welcome to ReState</Text>
+
+      {/* Test 3: Combined */}
+      <Text className="my-2 text-3xl font-rubik-bold">Test 3: Bold Weight</Text>
+
+      {/* Test 4: Different font weights */}
+      <Text className="my-2 text-3xl font-rubik-light">
+        Test 4: Light Weight
+      </Text>
 
       <Link href={"/sign-in"}>Sign In</Link>
-      <Link href={"/explore"}>Explore</Link>
+      <Link href={"/explore"}>exploreeeeeeee</Link>
       <Link href={"/profile"}>Profile</Link>
       <Link href={"/properties/1"}>Properties</Link>
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) console.log("Error loading fonts:", error);
+    if (fontsLoaded) {
+      console.log("Fonts loaded successfully");
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>Loading fonts...</Text>
+      </View>
+    );
+  }
+
   return <Stack />;
 }
