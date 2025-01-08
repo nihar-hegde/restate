2d1c41e8471eee8e55497170ac1a7bac153908a1
Nihar Hegde <niharhegde163@gmail.com>
2025-01-08 21:53:12 +0530

    simple headdre component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,27 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="p-4">
-      <Text className="font-rubik text-3xl my-10">Welcome to ReState</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5 ">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300 ">
+                Nihar
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
