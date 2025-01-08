bbaf0c512fe4eb0c13390a36c5b6f85323bd6d2a
Nihar Hegde <niharhegde163@gmail.com>
2025-01-08 22:06:25 +0530

    add a search component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Image, Text, View } from "react-native";
@@ -22,6 +23,7 @@ export default function Index() {
           <Image source={icons.bell} className="size-6" />
         </View>
       </View>
+      <Search />
     </SafeAreaView>
   );
 }
