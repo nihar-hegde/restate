24daa6034d154b3b4c0defabb4dc6b25e51089d1
Nihar Hegde <niharhegde163@gmail.com>
2025-01-11 22:54:13 +0530

    create filters

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filter";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -53,6 +54,7 @@ export default function Index() {
             </Text>
           </TouchableOpacity>
         </View>
+        <Filters />
         <View className="flex flex-row gap-5 mt-5">
           <Card />
           <Card />
