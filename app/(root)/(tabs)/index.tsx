de812cf215d0e9545c4fcb7ac8cf6dd385367d94
Nihar Hegde <niharhegde163@gmail.com>
2025-01-11 22:37:02 +0530

    card components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -36,10 +36,27 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+
+          <View className="flex flex-row gap-5 mt-5 ">
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
         </View>
 
-        <FeaturedCard />
-        <Card />
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300 ">
+            Our Recomendations.
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300 ">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
+        </View>
       </View>
     </SafeAreaView>
   );
