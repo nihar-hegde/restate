b626fde722e170f0b8273d047ab62a27be94246d
Nihar Hegde <niharhegde163@gmail.com>
2025-01-11 22:21:26 +0530

    basic featured card impelmentation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,13 +1,14 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5 ">
+      <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row">
             <Image source={images.avatar} className="size-12 rounded-full" />
@@ -22,8 +23,24 @@ export default function Index() {
           </View>
           <Image source={icons.bell} className="size-6" />
         </View>
+
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300 ">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300 ">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <FeaturedCard />
+        <Card />
       </View>
-      <Search />
     </SafeAreaView>
   );
 }
